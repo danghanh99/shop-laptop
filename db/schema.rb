d3d1135240a6735ce038dd07651121fa57e87df1
danghanh <ldhanh1999@gmail.com>
2020-11-01 18:42:40 +0700

    config to use db pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_01_104716) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_11_01_104716) do
     t.integer "quantity"
     t.integer "ram"
     t.integer "memory"
-    t.integer "category_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_products_on_category_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+hp = Category.create_with( name: 'HP').find_or_create_by(name: 'HP' )
+dell = Category.create_with( name: 'DELL', description: 'strong').find_or_create_by(name: 'DELL' )
+macbook = Category.create_with( name: 'MACBOOK', description: 'expensive').find_or_create_by(name: 'MACBOOK' )
+
+hp_1 = Product.create_with(
+  category_id: hp.id,
+  name: 'HP 2020',
+  price: 25_000_000,
+  quantity: 10,
+  ram: 16,
+  memory: 256,
+).find_or_create_by(name: 'HP 2020')
+
+hp_2 = Product.create_with(
+  category_id: hp.id,
+  name: 'HP pro 2020',
+  price: 30_000_000,
+  quantity: 15,
+  ram: 16,
+  memory: 512,
+).find_or_create_by(name: 'HP pro 2020')
+
+dell_1 = Product.create_with(
+  category_id: dell.id,
+  name: 'Dell 2020',
+  price: 25_000_000,
+  quantity: 20,
+  ram: 16,
+  memory: 256,
+).find_or_create_by(name: 'Dell 2020')
+
+dell_2 = Product.create_with(
+  category_id: dell.id,
+  name: 'Dell pro 2020',
+  price: 25_000_000,
+  quantity: 25,
+  ram: 16,
+  memory: 256,
+).find_or_create_by(name: 'Dell pro 2020')
+
+macbook_1 = Product.create_with(
+  category_id: macbook.id,
+  name: 'Macbook pro 2020',
+  price: 55_000_000,
+  quantity: 5,
+  ram: 32,
+  memory: 512,
+).find_or_create_by(name: 'Macbook pro 2020')
