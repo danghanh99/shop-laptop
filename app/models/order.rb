f688a3db26b0470f472bf6ea4d64a2863d82b5d5
quanghuy <huytran.301099@gmail.com>
2020-11-11 15:43:38 +0700

    add hisory order customer

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,11 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
   enum status: { pending: 'pending', shipping: 'shipping',shipped: 'shipped', cancelled: 'cancelled' }
+  validates :status, presence: true, inclusion: { in: %w[pending shipping shipped cancelled] }
   validates :subtotal, numericality: { greater_than_or_equal_to: 0 }
+
+  def self.search(params)
+    orders = Order.all
+    orders.where({ user_id: params[:id].presence, status: params[:status].presence }.compact)
+  end
 end
