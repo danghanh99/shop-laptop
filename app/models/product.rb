f93c183f724f663dcfbd21f63341ef14c630e8e7
danghanh <ldhanh1999@gmail.com>
2020-11-13 15:28:36 +0700

    remove uniquee name product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
   attr_accessor :picture
