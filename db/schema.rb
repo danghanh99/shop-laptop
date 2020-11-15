ce20c58ef5cee13419d409d54838f1b83afdef4e
danghanh <ldhanh1999@gmail.com>
2020-11-15 17:53:17 +0700

    add column description to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_162610) do
+ActiveRecord::Schema.define(version: 2020_11_15_105223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_11_09_162610) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "picture"
+    t.text "description"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
