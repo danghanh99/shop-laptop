96b67b8324fc7432d81361ccb345bd7a6234b559
danghanh <ldhanh1999@gmail.com>
2020-11-16 15:54:08 +0700

    delete product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   validates :name, presence: true
+  has_many :order_items, dependent: :destroy
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
   attr_accessor :picture
