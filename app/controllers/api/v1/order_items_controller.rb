49bac42d8fa842b68dd633221e0b969ca981cb37
danghanh <ldhanh1999@gmail.com>
2020-12-04 20:30:18 +0700

    handle add to cart the same product

--- app/controllers/api/v1/order_items_controller.rb
+++ app/controllers/api/v1/order_items_controller.rb
@@ -11,8 +11,10 @@ class Api::V1::OrderItemsController < ApplicationController
   end
 
   def create
-    order_item = OrderItem.new(order_item_params)
-    order_item.save!
+    cart = Cart.find(order_item_params[:cart_id])
+    order_item = cart.order_items.find_by product_id: order_item_params[:product_id]
+    order_item.update! quantity: order_item.quantity + 1 if order_item.present?
+    order_item = OrderItem.create!(order_item_params) unless order_item.present?
     render_resource order_item, :created
   end
 
