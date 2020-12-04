a1e8d1dbe7701e5cc75f389d865445b16664da8a
danghanh <ldhanh1999@gmail.com>
2020-12-04 22:39:54 +0700

    revenue money

--- app/controllers/api/v1/statistics_controller.rb
+++ app/controllers/api/v1/statistics_controller.rb
@@ -16,12 +16,16 @@ class Api::V1::StatisticsController < ApplicationController
     }, status: :ok
   end
 
-  # def count_revenue
-  #   user_quantity = User.count
-  #   render json: {
-  #     user_quantity: user_quantity,
-  #   }, status: :ok
-  # end
-
+  def count_revenue
+    orders = Order.where status: 'shipped'
+    orders = orders.where('extract(year  from created_at) = ?', params[:year]) if params[:year].present?
+    orders = orders.where('extract(month from created_at) = ?', params[:month]) if params[:month].present?
+    response = Hash.new()
+    response[:year] = params[:year] || 0
+    response[:month] = params[:month] || 0
+    response[:order_quantity] = orders.count || 0
+    response[:revenue] = orders.pluck(:subtotal).inject{ |item,sum| sum+= item} || 0
+    render json: response, status: :ok
+  end
   
 end
