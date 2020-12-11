6c447417d783c1e2936c617a99e648478a4c53b7
danghanh <ldhanh1999@gmail.com>
2020-12-11 23:21:23 +0700

    HANH

--- app/serializers/order_item_serializer.rb
+++ app/serializers/order_item_serializer.rb
@@ -1,4 +1,4 @@
 class OrderItemSerializer < ActiveModel::Serializer
-  attributes :id, :product_name, :unit_price, :quantity, :cart_id, :order_id, :product_id
+  attributes :id, :product_name, :unit_price, :quantity, :cart_id, :order_id, :product_id, :picture
   has_one :product
 end
